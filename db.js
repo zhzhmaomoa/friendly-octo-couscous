88776801f8bfc2546d0c091f90b25066c4320562
liuzhizhen <l742802447@163.com>
2023-10-07 15:05:49 +0800

    map markers

--- db.js
+++ db.js
@@ -27,7 +27,23 @@ export const Member = sequelize.define("Member",{
   },
   name:{
     type:DataTypes.STRING,
-    allowNull:false
+    allowNull:false,
+    comment:'族员名字'
+  },
+  latitude:{
+    type:DataTypes.FLOAT,
+    allowNull:false,
+    comment:'纬度'
+  },
+  longitude:{
+    type:DataTypes.FLOAT,
+    allowNull:false,
+    comment:'经度'
+  },
+  iconPath:{
+    type:DataTypes.STRING,
+    allowNull:false,
+    comment:"形象"
   }
 })
 export const Contribution = sequelize.define(
